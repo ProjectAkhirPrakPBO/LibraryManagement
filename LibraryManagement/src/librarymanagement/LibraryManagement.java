bb73a290c55ac1a7ec4961f1f26d3035f6ed2dc9
Rex4Red <putrasananda61@gmail.com>
2025-05-08 12:31:22 +0700

    tes

--- LibraryManagement/src/librarymanagement/LibraryManagement.java
+++ LibraryManagement/src/librarymanagement/LibraryManagement.java
@@ -15,7 +15,7 @@ public class LibraryManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        halo
+        halo haloo
     }
     
 }
